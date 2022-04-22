0707f665de6233eac1333d7cf78b918a39f1f96c
Jonathan Fisher <jonathan@trueanthem.com>
2022-04-21 18:29:10 -0700

    add gitignore

--- server.js
+++ server.js
@@ -18,11 +18,22 @@ app.get('/(:id)', function(request, response) {
 
 //get sharejs dependencies
 const sharejs = require('share');
-require('redis');
+
+//set up redis server
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+    redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+    redisClient = require("redis").createClient();
+}
+
 
 //options for sharejs
 const options = {
-    db: {type: 'redis'},
+    db: {type: 'redis', client: redisClient},
 };
 
 //attach express server to sharejs
